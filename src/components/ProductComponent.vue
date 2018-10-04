91133a53d6b9c4f2ea98d250a7e0abb6fa8f54e0
6Jonathan6 <jpr1993mm@hotmail.com>
2018-10-04 00:02:11 -0500

    correct product rendering

--- src/components/ProductComponent.vue
+++ src/components/ProductComponent.vue
@@ -24,44 +24,95 @@
 
 
                                 <!-- After negative  0 < cantidad < minimo -->
-                                <td :class="isLessThanMinimum">
-                                    <div class="line negative first" >
-                                        <div class="number">{{  producto.cantidad < producto.minimo ? producto.cantidad : null  }}</div>
+                                <td class="px-0 range long"   v-if="isPositiveAndLessThanMinimum">
+                                    <div class="line negative first">
+                                        <div class="number">{{  producto.cantidad }}</div>
                                     </div>
                                 </td>
 
+                                 <!-- After negative  0 < cantidad < minimo  === false -->
+                                <td class="px-0 range long" v-if="!isPositiveAndLessThanMinimum">
+                                    <div class="line first"></div>
+                                </td>
 
+
+                                <!-- minimo circle -->
                                 <td class="px-0 range index">
                                     <div class="index">
                                         <div class="number">{{producto.minimo}}</div>
                                     </div>
                                 </td>
-                                <td class="px-0 range">
+
+
+                                <!-- minimo < cantidad < punto reorden -->
+                                <td class="px-0 range" v-if="isGreaterThanMinAndLesThanPo">
+                                    <div class="line warning">
+                                        <div class="number">{{producto.cantidad}}</div>
+                                    </div>
+                                </td>
+                                <td class="px-0 range" v-if="isGreaterThanMinAndLesThanPo">
+                                    <div class="line warning"></div>
+                                </td>
+
+                                <!-- minimo < cantidad < punto reorden === false -->
+                                <td class="px-0 range" v-if="!isGreaterThanMinAndLesThanPo">
                                     <div class="line"></div>
                                 </td>
-                                <td class="px-0 range">
+                                <td class="px-0 range" v-if="!isGreaterThanMinAndLesThanPo">
                                     <div class="line"></div>
                                 </td>
+
+
+                                <!-- Markup for porden circle porden=punto de reorden -->
                                 <td class="px-0 range index">
                                     <div class="index">
                                         <div class="number">{{producto.porden}}</div>
                                     </div>
                                 </td>
-                                <td class="px-0 range">
+
+
+                                <!-- porden < cantidad < maximo -->
+                                <td class="px-0 range" v-if="isGreaterThanPoAndLessThanMax">
+                                    <div class="line positive">
+                                        <div class="number">{{producto.cantidad}}</div>
+                                    </div>
+                                </td>
+                                <td class="px-0 range" v-if="isGreaterThanPoAndLessThanMax">
+                                    <div class="line positive"></div>
+                                </td>
+
+                                <!-- porden < cantidad < maximo === false -->
+                                <td class="px-0 range" v-if="!isGreaterThanPoAndLessThanMax">
                                     <div class="line"></div>
                                 </td>
-                                <td class="px-0 range">
+                                <td class="px-0 range" v-if="!isGreaterThanPoAndLessThanMax">
                                     <div class="line"></div>
                                 </td>
+
+
+                                <!-- Maximo circle -->
                                 <td class="px-0 range index">
                                     <div class="index">
                                         <div class="number">{{producto.maximo}}</div>
                                     </div>
                                 </td>
-                                <td class="px-0 range">
+
+
+                                <!-- maximo < cantidad -->
+                                <td class="px-0 range" v-if="isGreaterThanMax">
+                                    <div class="line positive max">
+                                        <div class="number">{{producto.cantidad}}</div>
+                                    </div>
+                                </td>
+                                <td class="px-0 range" v-if="isGreaterThanMax">
+                                    <div class="line positive last"></div>
+                                </td>
+
+                                <!-- maximo < cantidad ===false -->
+                                <td class="px-0 range" v-if="!isGreaterThanMax">
                                     <div class="line"></div>
                                 </td>
-                                <td class="px-0 range">
+                                <td class="px-0 range" v-if="!isGreaterThanMax">
                                     <div class="line last"></div>
                                 </td>
                             </tr>
@@ -79,29 +130,44 @@ export default {
       required: true
     }
   },
-  computed: {
-    isNegativeObject() {
-      const isNegative = this.producto.cantidad < 0;
-      const negativeObject = {
+  data() {
+    return {
+      negativeObject: {
         range: true,
         one: true,
         none: false
-      };
-      const positiveObject = {
+      },
+      positiveObject: {
         range: true,
         one: true,
         none: true
-      };
-      return isNegative ? negativeObject : positiveObject;
-    }
+      }
+    };
   },
-  isLessThanMinimum() {
-    const isLess = { "px-0": true, range: true, long: true, none: false };
-    const isNotLess = { "px-0": true, range: true, long: true, none: true };
-    return 0 < this.producto.cantidad &&
-      this.producto.cantidad < this.propducto.minimo
-      ? isLess
-      : isNotLess;
+  computed: {
+    isNegativeObject() {
+      const isNegative = this.producto.cantidad < 0;
+      return isNegative ? this.negativeObject : this.positiveObject;
+    },
+    isPositiveAndLessThanMinimum() {
+      const isPositive = 0 < this.producto.cantidad;
+      const isLessThanMinimum = this.producto.cantidad < this.producto.minimo;
+      return isPositive && isLessThanMinimum;
+    },
+    isGreaterThanMinAndLesThanPo() {
+      const isGreaterThanMinimum =
+        this.producto.minimo < this.producto.cantidad;
+      const isLessThanPo = this.producto.cantidad < this.producto.porden;
+      return isGreaterThanMinimum && isLessThanPo;
+    },
+    isGreaterThanPoAndLessThanMax() {
+      const isGreaterThanPo = this.producto.porden < this.producto.cantidad;
+      const isLessThanMax = this.producto.cantidad < this.producto.maximo;
+      return isGreaterThanPo && isLessThanMax;
+    },
+    isGreaterThanMax() {
+      return this.producto.cantidad > this.producto.maximo;
+    }
   }
 };
 </script>
